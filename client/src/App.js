87987b261d44238c0f8e13ba5b18b1d2d9fbd2fd
misha novakishvili <46324818+misza777@users.noreply.github.com>
2021-06-23 22:46:24 +0200

    betteration2

--- client/src/App.js
+++ client/src/App.js
@@ -44,15 +44,31 @@ const App = () => {
           <section className="container">
             <Alert />
             <Switch>
-              <Route path="/register" component={Register} />
-              <Route path="/login" component={Login} />
-              <Route path="/profiles" component={Profiles} />
-              <Route path="/profile/:id" component={Profile} />
-              <PrivateRoute path="/dashboard" component={Dashboard} />
-              <PrivateRoute path="/create-profile" component={CreateProfile} />
-              <PrivateRoute path="/edit-profile" component={EditProfile} />
-              <PrivateRoute path="/add-experience" component={AddExperience} />
-              <PrivateRoute path="/add-education" component={AddEducation} />
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/profiles" component={Profiles} />
+              <Route exact path="/profile/:id" component={Profile} />
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute
+                exact
+                path="/create-profile"
+                component={CreateProfile}
+              />
+              <PrivateRoute
+                exact
+                path="/edit-profile"
+                component={EditProfile}
+              />
+              <PrivateRoute
+                exact
+                path="/add-experience"
+                component={AddExperience}
+              />
+              <PrivateRoute
+                exact
+                path="/add-education"
+                component={AddEducation}
+              />
               <PrivateRoute exact path="/posts" component={Posts} />
               <PrivateRoute exact path="/posts/:id" component={Post} />
             </Switch>
